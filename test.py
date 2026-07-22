d8263d174d5d33499c45f84d9553a22332ec0ad3
ehsanmahdavifar86 <ehsan.mahdavifar86@gmail.com>
2026-07-22 11:04:56 +0330

    django started

--- test.py
+++ test.py
@@ -1,19 +1,5 @@
-# Ehsan Mahdavifar
-# 404555055
+import os
 
-lst_season = []
-
-string = open('test.txt').read()
-
-for line in string.split('\n'):
-    lst_season.append(line)
-
-count = 0
-for line in open('test.txt'):
-    if line == 'whale' or 'whales':
-        count += 1
-
-words = []
-for x in lst_season:
-    while lst_season == '16':
-        words.append(x)
+import django
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "project1.settings")
+django.setup()

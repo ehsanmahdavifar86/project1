4577fefd3f706c048034c32f396b0932cd56aa4a
ehsanmahdavifar86 <ehsan.mahdavifar86@gmail.com>
2026-08-05 11:58:27 +0330

    created first app
    created first app in the name of website add changing the path of urls and views from the project folder to the app folder.

--- views.py
+++
@@ -1,4 +0,0 @@
-from django.http import HttpResponse
-
-def http_test(request):
-    return HttpResponse('<h1>this is a test</h1>')
--- website/views.py
+++ website/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+def index_view(request):
+    return HttpResponse('<h1>Home page</h1>')
+
+def about_view(request):
+    return HttpResponse('<h1>About page</h1>')
+
+def contact_view(request):
+    return HttpResponse('<h1>Contact us</h1>')
